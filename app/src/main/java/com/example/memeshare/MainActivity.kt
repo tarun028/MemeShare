b727a764e4567afd7d0e1d34b50ece625919fd36
tarun028 <tarunsrivastavakvs@gmail.com>
2023-09-01 16:03:30 +0530

    XML DONE

---
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.memeshare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun ShareMeme(view: View) {
+
+    }
+    fun nextMeme(view: View) {
+
+    }
+}

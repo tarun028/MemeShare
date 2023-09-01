9720f2d73f3338b0a3c9acbb8ebdd0ebec4e1d82
tarun028 <tarunsrivastavakvs@gmail.com>
2023-09-01 16:35:02 +0530

    Volley

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -2,15 +2,40 @@ package com.example.memeshare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadMeme()
     }
 
-    fun ShareMeme(view: View) {
+    private fun loadMeme(){
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://www.google.com"
+
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                // Display the first 500 characters of the response string.
+                Log.d("success Request", response.substring(0, 500))
+            },
+            Response.ErrorListener {
+                Log.d("error", it.localizedMessage)
+            })
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
+    }
+
+    fun shareMeme(view: View) {
 
     }
     fun nextMeme(view: View) {
